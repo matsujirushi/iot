33a644618d1726e1a083d328a595c8489504e6c9
Takashi Matsuoka <matsujirushi@live.jp>
2022-02-06 21:56:14 +0900

    Create Scd30

---
+++ src/devices/Scd30/tests/Scd30Tests.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Iot.Device.Scd30.Tests
+{
+    public class Scd30Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // NOTE: THIS TEST PROJECT IS NOT REQUIRED AND CAN BE DELETED IF NOT USED.
+        }
+    }
+}
